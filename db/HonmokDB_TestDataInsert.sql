bb9e970fd6f017bd91245acfc54d56efd2302341
Koji HIJIKURO <koji0903@gmail.com>
2014-03-10 15:15:29 +0900

    貸し出しテーブルに対するテスト情報も追加

--- db/HonmokDB_TestDataInsert.sql
+++ db/HonmokDB_TestDataInsert.sql
@@ -12,7 +12,7 @@ INSERT INTO users ( userNo, uname, address, tel ) VALUES ( 100000, '肥後　太
 INSERT INTO users ( userNo, uname, address, tel ) VALUES ( 100001, '大阿蘇 次郎', '阿蘇888-456', '0120-1515-4231' );
 INSERT INTO users ( userNo, uname, address, tel ) VALUES ( 100008, '水前寺　花子', '水前寺888-456', '0120-3121-9999' );
 
--- bookデータを作成
+-- book/libraryBooksデータを作成
 SET @d = 0;
 
 INSERT INTO books ( bname, author, publisher ) VALUES ( 'フェイスブックをつくったザッカーバーグの仕事術', '桑原晃弥', '幻冬舎' );
@@ -30,3 +30,12 @@ INSERT INTO libraryBooks ( bid, bookNo ) VALUES ( @d, 200002 );
 INSERT INTO books ( bname, author, publisher ) VALUES ( '羅生門', '芥川竜之介', '帝国文学' );
 SET @d = (SELECT bid FROM books WHERE bname = '羅生門' );
 INSERT INTO libraryBooks ( bid, bookNo ) VALUES ( @d, 200003 );
+
+-- circulationsデータを作成
+
+-- 返却済み
+INSERT INTO circulations ( issueDay, returnDay, uid, lbid ) VALUES ( '2014-03-10 13:49:22', '2014-03-10 15:49:22', 1, 1);
+-- 貸し出し中
+INSERT INTO circulations ( issueDay, uid, lbid ) VALUES ( '2014-03-11 15:49:22', 2, 2);
+-- 貸し出し＆延滞
+INSERT INTO circulations ( issueDay, uid, lbid ) VALUES ( '2014-02-01 15:49:22', 3, 3);

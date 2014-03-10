a68679ba675fa712821f5fb61e1d8a088c927fcd
Makoto Sakuragi <makoragi@gmail.com>
2014-03-10 16:41:41 +0900

    サーブレットとJSP()のサンプルを追加

---
+++ src/serv/RentController.java
@@ -0,0 +1,32 @@
+package serv;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/rent")
+public class RentController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RentController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher dispatch = request.getRequestDispatcher("rent.jsp");
+		dispatch.forward(request, response);
+	}
+
+}

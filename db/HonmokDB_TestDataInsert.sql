aa61e4c5dcee8d358fc4b8e83a62e559dfc402d3
Koji HIJIKURO <koji0903@gmail.com>
2014-03-10 14:49:45 +0900

    データベース構成の見直し（DB仕様 ver0.3）＆テストデータ用SQL作成

---
+++ db/HonmokDB_TestDataInsert.sql
@@ -0,0 +1,32 @@
+-- 既存データを削除
+DELETE from users;
+DELETE from books;
+DELETE from libraryBooks;
+DELETE from circulations;
+
+
+SET NAMES utf8;
+
+-- userデータを作成
+INSERT INTO users ( userNo, uname, address, tel ) VALUES ( 100000, '肥後　太郎', '肥後123-456', '0120-1515-3776' );
+INSERT INTO users ( userNo, uname, address, tel ) VALUES ( 100001, '大阿蘇 次郎', '阿蘇888-456', '0120-1515-4231' );
+INSERT INTO users ( userNo, uname, address, tel ) VALUES ( 100008, '水前寺　花子', '水前寺888-456', '0120-3121-9999' );
+
+-- bookデータを作成
+SET @d = 0;
+
+INSERT INTO books ( bname, author, publisher ) VALUES ( 'フェイスブックをつくったザッカーバーグの仕事術', '桑原晃弥', '幻冬舎' );
+SET @d = (SELECT bid FROM books WHERE bname = 'フェイスブックをつくったザッカーバーグの仕事術' );
+INSERT INTO libraryBooks ( bid, bookNo ) VALUES ( @d, 200000 );
+
+INSERT INTO books ( bname, author, publisher ) VALUES ( 'あなたへの伝言', '高田郁', '幻冬舎' );
+SET @d = (SELECT bid FROM books WHERE bname = 'あなたへの伝言' );
+INSERT INTO libraryBooks ( bid, bookNo ) VALUES ( @d, 200001 );
+
+INSERT INTO books ( bname, author, publisher ) VALUES ( '三毛猫ホームズは階段を上る', '赤川次郎', '光文社文庫' );
+SET @d = (SELECT bid FROM books WHERE bname = '三毛猫ホームズは階段を上る' );
+INSERT INTO libraryBooks ( bid, bookNo ) VALUES ( @d, 200002 );
+
+INSERT INTO books ( bname, author, publisher ) VALUES ( '羅生門', '芥川竜之介', '帝国文学' );
+SET @d = (SELECT bid FROM books WHERE bname = '羅生門' );
+INSERT INTO libraryBooks ( bid, bookNo ) VALUES ( @d, 200003 );
--- db/HonmokDB_create.sql
+++ db/HonmokDB_create.sql
@@ -19,11 +19,11 @@ USE honmokdb;
  *
  ************************************************************/
 
--- userテーブル
+-- usersテーブル
 CREATE TABLE users
 (
 	uid INT PRIMARY KEY NOT NULL AUTO_INCREMENT, 
-	rid INT NOT NULL UNIQUE, 
+	userNo VARCHAR(20) NOT NULL UNIQUE, 
 	uname VARCHAR(50) NOT NULL,
 	address VARCHAR(50) NOT NULL,
 	tel VARCHAR(30) NOT NULL,
@@ -31,17 +31,27 @@ CREATE TABLE users
 	deleteFlag BOOLEAN NOT NULL DEFAULT FALSE
 );
 
--- bookテーブル
+-- booksテーブル
 CREATE TABLE books
 (
 	bid INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-	isbn INT,
+	isbn VARCHAR(20),
 	bname VARCHAR(30) NOT NULL,
 	author VARCHAR(30) NOT NULL,
 	publisher VARCHAR(30) NOT NULL,
 	page INT,
 	deleteFlag BOOLEAN NOT NULL DEFAULT FALSE
 );
+
+-- libraryBooksテーブル
+CREATE TABLE libraryBooks
+(
+	lbid INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	bid INT NOT NULL,
+	bookNo VARCHAR(20) NOT NULL UNIQUE,
+	deleteFlag BOOLEAN NOT NULL DEFAULT FALSE
+);
+
 -- circulationテーブル
 CREATE TABLE circulations
 (
@@ -49,7 +59,7 @@ CREATE TABLE circulations
 	issueDay DATETIME NOT NULL,
 	returnDay DATETIME,
 	uid INT NOT NULL,
-	bid INT NOT NULL,
+	lbid INT NOT NULL,
 	term INT DEFAULT 7,
 	deleteFlag BOOLEAN NOT NULL DEFAULT FALSE
 );

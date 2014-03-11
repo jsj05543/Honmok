910cb3e6ebab2a8415842f0883d8541bc3cb09b3
naoki-o <naoki.otani.19830617@gmail.com>
2014-03-11 13:06:49 +0900

    利用者IDの検索を機能をテスト実装

---
+++ src/serv/ReturnResultController.java
@@ -0,0 +1,58 @@
+package serv;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RentResult
+ */
+@WebServlet(description = "userNo, bookNoを受け取り、貸し出し処理を行う。", urlPatterns = { "/rent_result" })
+public class ReturnResultController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ReturnResultController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+		UserDB userdb = new UserDB();
+		User user = new User();
+
+		request.setCharacterEncoding("UTF-8");
+		String uid = request.getParameter("uid");
+		String bid = request.getParameter("bid");
+
+	    response.setContentType("text/html;charset=UTF-8");
+	    PrintWriter out = response.getWriter();
+
+
+		user = userdb.getUserDetail(uid);
+
+		if( user == null ){
+			out.println("該当しゃなし");
+		}else{
+			out.println(user.getUname());
+		}
+
+
+
+
+	}
+
+}
